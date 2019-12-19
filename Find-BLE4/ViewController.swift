7800c4424fd0f49d42240e9db3278c45ecec11cf
Mao Sasakura <huuchitaro@gmail.com>
2019-12-19 10:07:03 +0900

    Initial Commit

---
+++ Find-BLE4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find-BLE4
+//
+//  Created by 笹倉まお on 2019/12/19.
+//  Copyright © 2019年 笹倉まお. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
